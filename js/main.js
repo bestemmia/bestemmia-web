221cc25720b65a739c8568b40ce3e62d388e78da
netrace <giorgiomarzianidepaolis@Giorgio-Marziani-de-Paolis.local>
2015-10-21 10:30:48 +0200

    repo and fix initial commits

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
 });
 
 (function() {
-  var flickerAPI = "https://api.github.com/repos/videojs/video-js-swf/commits";
+  var flickerAPI = "https://api.github.com/repos/bestemmia/bestemmia-qui/commits";
   $.getJSON(flickerAPI)
     .done(function( data ) {
+      data.pop();
+      data.pop();
       $.each( data, function( i, item ) {
-		    if(item.author){
-        $("#textMachine").append( "<div><img style=\"height:60px;\" src=\""+item.author.avatar_url+"\" />"+item.commit.message+"</div>" );
+        if(item.author){
+            $("#textMachine").append( "<div><img style=\"height:60px;\" src=\""+item.author.avatar_url+"\" />"+i+item.commit.message+"</div>" );
         }
       });
 	  $("#textMachine").slotMachine({
